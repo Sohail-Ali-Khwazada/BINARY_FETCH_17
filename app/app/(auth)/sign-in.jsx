43109e43076949b4f7b015050162564380c62476
Sohail Ali Khwazada <sohailali.khwazada2011@gmail.com>
2024-09-30 18:43:32 +0530

    fixed the styling of sign in and signup page

--- app/app/(auth)/sign-in.jsx
+++ app/app/(auth)/sign-in.jsx
@@ -1,20 +1,67 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
-import { router } from "expo-router";
+import React, { useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity, ScrollView, Alert, Image } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { icons } from '../../constants';
+import { router } from 'expo-router';
 
-const SignIn = () => {
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {
+    if (!email || !password) {
+      Alert.alert('Please fill in all fields.');
+      return;
+    }
+    console.log({ email, password });
+    Alert.alert('Sign In Successful!');
+  };
+
   return (
-    <View>
-      <TouchableOpacity
-        className="bg-[#5c5cb0] py-4 rounded-full shadow-lg my-12"
-        onPress={() => router.push("/home")}
-      >
-        <Text className="text-center text-lg font-psemibold text-white">
-          home
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+    <ScrollView className="flex-1 bg-primary p-4 pt-20">
+      <StatusBar style="light" />
+      <View className="flex-col items-center justify-center w-full">
+        <Image source={icons.logo} className="w-32 h-32 mb-4" resizeMode='contain' />
+        <Text className="text-3xl font-bold text-center mb-8 text-secondary-100">Sign In</Text>
+        
+        <View className="mb-4 w-full">
+          <Text className="text-gray-300 mb-2">Email</Text>
+          <TextInput 
+            className="border border-gray-600 bg-black-200 text-white p-3 rounded-lg shadow-md"
+            placeholder="Enter your email"
+            keyboardType="email-address"
+            placeholderTextColor="#CDCDE0"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+        
+        <View className="mb-4 w-full">
+          <Text className="text-gray-300 mb-2">Password</Text>
+          <TextInput 
+            className="border border-gray-600 bg-black-200 text-white p-3 rounded-lg shadow-md"
+            placeholder="Enter your password"
+            secureTextEntry
+            placeholderTextColor="#CDCDE0"
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        
+        <TouchableOpacity 
+          className="bg-secondary-100 p-3 rounded-lg shadow-md w-full"
+          onPress={handleSignIn}
+        >
+          <Text className="text-white text-center font-semibold text-lg">Sign In</Text>
+        </TouchableOpacity>
 
-export default SignIn;
+        <TouchableOpacity className="mt-4" onPress={() => router.push('/sign-up')}>
+          <Text className="text-center text-gray-400">
+            Don't have an account?{' '}
+            <Text className="text-secondary-100 font-semibold">Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
--- app/app/(auth)/sign-up.jsx
+++ app/app/(auth)/sign-up.jsx
@@ -5,9 +5,8 @@ import { icons } from '../../constants';
 import { router } from 'expo-router';
 import { useGlobalContext } from './../../context/GlobalProvider';
 
-
 export default function SignUp() {
-  const { setUser } = useGlobalContext(); 
+  const { setUser } = useGlobalContext(); // Context to manage user state
   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -19,24 +18,24 @@ export default function SignUp() {
     }
 
     try {
-      const response = await axios.post('https://6nddmv2g-5000.inc1.devtunnels.ms/api/auth/signup', { 
-        fullName, 
-        email, 
-        password 
-      });
-
-      const res=await fetch('https://6nddmv2g-5000.inc1.devtunnels.ms/api/auth/signup', {
+      const response = await fetch('https://6nddmv2g-5000.inc1.devtunnels.ms/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ fullName, email, password }),
       });
-      const data =await res.json();
-      
-      console.log(data);
-      // Alert.alert('Word');
 
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(data);
+        setUser(data); 
+        router.push('/home'); 
+      } else {
+        throw new Error(data.message || 'Sign Up Failed');
+      }
+      
     } catch (err) {
       console.error(err); // Log the error for debugging
       Alert.alert('Sign Up Failed!', err.message || 'Please try again later.');
@@ -44,55 +43,58 @@ export default function SignUp() {
   };
 
   return (
-    <ScrollView className="bg-white p-4 pt-20 flex-1">
-      <StatusBar style="dark" />
+    <ScrollView className="flex-1 bg-primary p-4 pt-20">
+      <StatusBar style="light" />
       <View className="flex-col items-center justify-center w-full">
         <Image source={icons.logo} className="w-32 h-32 mb-4" resizeMode='contain' />
-        <Text className="text-3xl font-bold text-center mb-8 text-blue-600">Create Account</Text>
+        <Text className="text-3xl font-bold text-center mb-8 text-secondary-100">Create Account</Text>
         
         <View className="mb-4 w-full">
-          <Text className="text-gray-700 mb-2">Full Name</Text>
+          <Text className="text-gray-200 mb-2">Full Name</Text>
           <TextInput 
-            className="border border-gray-300 p-3 rounded-lg shadow-md"
+            className="border border-gray-300 bg-black-200 text-white p-3 rounded-lg shadow-md"
             placeholder="Enter your full name"
+            placeholderTextColor="#CDCDE0"
             value={fullName}
             onChangeText={setFullName}
           />
         </View>
         
         <View className="mb-4 w-full">
-          <Text className="text-gray-700 mb-2">Email</Text>
+          <Text className="text-gray-200 mb-2">Email</Text>
           <TextInput 
-            className="border border-gray-300 p-3 rounded-lg shadow-md"
+            className="border border-gray-300 bg-black-200 text-white p-3 rounded-lg shadow-md"
             placeholder="Enter your email"
             keyboardType="email-address"
+            placeholderTextColor="#CDCDE0"
             value={email}
             onChangeText={setEmail}
           />
         </View>
         
         <View className="mb-4 w-full">
-          <Text className="text-gray-700 mb-2">Password</Text>
+          <Text className="text-gray-200 mb-2">Password</Text>
           <TextInput 
-            className="border border-gray-300 p-3 rounded-lg shadow-md"
+            className="border border-gray-300 bg-black-200 text-white p-3 rounded-lg shadow-md"
             placeholder="Enter your password"
             secureTextEntry
+            placeholderTextColor="#CDCDE0"
             value={password}
             onChangeText={setPassword}
           />
         </View>
         
         <TouchableOpacity 
-          className="bg-blue-600 p-3 rounded-lg shadow-md w-full"
+          className="bg-secondary-100 p-3 rounded-lg shadow-md w-full"
           onPress={handleSignUp}
         >
           <Text className="text-white text-center font-semibold text-lg">Sign Up</Text>
         </TouchableOpacity>
 
         <TouchableOpacity className="mt-4" onPress={() => router.push('/sign-in')}>
-          <Text className="text-center text-gray-600">
+          <Text className="text-center text-gray-300">
             Already have an account?{' '}
-            <Text className="text-blue-600 font-semibold">Sign In</Text>
+            <Text className="text-secondary-100 font-semibold">Sign In</Text>
           </Text>
         </TouchableOpacity>
       </View>
